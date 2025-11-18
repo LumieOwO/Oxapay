1f8840dc1570a5f67e6c2ca4f94a76a26db0a1b0
Loomz <120320080+LumieOwO@users.noreply.github.com>
2025-11-19 00:49:35 +0200

    Refactor exports to named exports for clients

--- main.ts
+++ main.ts
@@ -1,14 +1,9 @@
-import V1Payment from './clients/v1/payment';
-import V1General from './clients/v1/general';
-import V1Payout from './clients/v1/payout';
-import V1Common from './clients/v1/common';
+import V1Payment from './clients/v1/payment'
+import V1General from './clients/v1/general'
+import V1Payout from './clients/v1/payout'
+import V1Common from './clients/v1/common'
 
-export default {
-    v1: {
-        payment: V1Payment,
-        general: V1General,
-        payout: V1Payout,
-        common: V1Common,
-    },
-
-}
+export const Payment = V1Payment
+export const General = V1General
+export const Payout = V1Payout
+export const Common = V1Common
